2bae4b18782eed983b55f3661c274abb1d0f86c0
andmigle-lang <and.migle@gmail.com>
2025-12-23 22:54:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,64 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <cctype>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <regex>
+
+using std::ifstream;
+using std::cout;
+using std::endl;
+using std::map;
+using std::string;
+using std::stringstream;
+using std::ofstream;
+using std::tolower;
+using std::right;
+using std::setw;
+using std::fixed;
+using std::vector;
+using std::left;
+using std::set;
+using std::regex;
+using std::smatch;
+using std::ostream;
+using std::cin;
+
+int utf8_ilgis(const string& zodis);
+void zodziu_ir_tarpu_spausdinimas(ostream& out, const string& zodis, int sk);
+
 int main() {
-	SetConsoleOutputCP(CP_UTF8); 
-	SetConsoleCP(CP_UTF8); 
-	locale::global(locale("en_US.UTF-8"));
-	locale utf8("en_US.UTF-8");
-
 	//1 ir 2 uzduotis - isvedama i ta pati faila rez.txt
-	ifstream F("Vilnius.txt");
-	F.imbue(utf8);
-
-	if (!F) {
-		cout << "Failas neatidarytas" << endl;
+	
+	string failo_pavadinimas;
+	ifstream F;
+	while (true) {
+		cout << "Iveskite failo pavadinima (Vilnius.txt): ";
+		cin >> failo_pavadinimas;
+		F.open(failo_pavadinimas);
+		if (F) {
+			break;
+		}
+		else {
+			string pasirink;
+			while (true) {
+				cout << "Failo nepavyko atidaryti. Ar norite bandyti dar karta (rasykite 1), ar norite uzbaigti programa (rasykite 2)? ";
+				cin >> pasirink;
+				if (pasirink == "1") {
+					break;
+				}
+				else if (pasirink == "2") {
+					exit(0);
+				}
+				else {
+					cout << "Neteisingas pasirinkimas. Bandykite dar karta." << endl;
+				}
+			}
+		}
 	}
 
 	map<string, int> zodziu_skaicius;
@@ -26,16 +75,34 @@ int main() {
 		while (ss >> zodis) {
 			string zodis_be_skyrybos;
 
-			for (unsigned char raide : zodis) {
-				if (isalpha(raide)) {
-					raide = tolower(raide);
-					zodis_be_skyrybos += raide;
+			for (size_t i = 0; i < zodis.size(); i++) {
+				unsigned char raide = static_cast<unsigned char>(zodis[i]);
+
+				//ASCII raide (vieno baito)
+				if (raide < 128) {
+					if (isalpha(raide)) {
+						raide = tolower(raide);
+						zodis_be_skyrybos += raide;
+					}
 				}
-				//ieskoma ne ASCII raidziu - t. y. tu, kuriose pirmas bitas yra 1 (bitine operacija)
-				else if (raide & 0x80) {
-					zodis_be_skyrybos += raide;
+				//UTF‑8 dvieju baitu raides (lietuviskos, rusiskos ir t.t.) 
+				//pirmas baitas turi forma 110xxxxx, t.y. (raide & 0xE0) == 0xC0
+				else if ((raide & 0xE0) == 0xC0) {
+					if (i + 1 < zodis.size()) {
+						unsigned char raide2 = static_cast<unsigned char>(zodis[i + 1]);
+
+						//antras (continuation) baitas uzsiraso forma 10xxxxxx
+						if ((raide2 & 0xC0) == 0x80) {
+							zodis_be_skyrybos += raide; //pirmas baitas
+							zodis_be_skyrybos += raide2; //antras baitas
+							i++; //praleidziame antra baita (continuation) ivesties cikle (kad nebutu perrasyta)
+						}
+					}
 				}
+				//3 baitu skyrybos zenklai ignoruojami
 			}
+
+
 			if (zodis_be_skyrybos != "") {
 				zodziu_skaicius[zodis_be_skyrybos]+=1;
 
@@ -47,7 +114,6 @@ int main() {
 	}
 	F.close();
 	ofstream G("rez.txt");
-	G.imbue(utf8);
 
 	G << left << setw(20) << "Zodis"
 		<< left << setw(10) << "Kiekis"
@@ -72,5 +138,108 @@ int main() {
 	}
 
 	G.close();
-    return 0;
+
+	//3 uzduotis
+	//istraukiame pletinius is failo
+	set<string> pletiniai;
+	ifstream T("URLs.txt");
+	string pletinys;
+
+	while (T >> pletinys) {
+		for (int i = 0; i < pletinys.size(); i++)
+			pletinys[i] = tolower(pletinys[i]);
+		pletiniai.insert(pletinys);
+	}
+	T.close();
+
+	//regex'as URL paieskai
+	regex url_regex(R"((https?://)?(www\.)?[A-Za-z0-9\.-]+(/[A-Za-z0-9\./\-\_\?\=\&]*)?)");
+
+	set<string> rasti_url;
+
+	ifstream F2(failo_pavadinimas);
+	string eilute_2;
+
+	while (getline(F2, eilute_2)) {
+
+		stringstream ss(eilute_2);
+		string zodis_2;
+
+		while (ss >> zodis_2) {
+
+			smatch match;
+			string url_kunas = "";
+
+			//bandoma surasti URL dali is zodzio naudojant regex'a
+			if (regex_search(zodis_2, match, url_regex)) {
+				url_kunas = match.str(0);
+			}
+			else {
+				continue; //nerastas URL
+			}
+
+			//padarome lowercase
+			string lowercase_zodis_2 = url_kunas;
+			for (int i = 0; i < lowercase_zodis_2.size(); i++)
+				lowercase_zodis_2[i] = tolower(lowercase_zodis_2[i]);
+
+			//tikriname ar tai yra URL pagal pletinius
+			int paskutinio_tasko_indeksas = -1;
+			for (int i = 0; i < lowercase_zodis_2.size(); i++) {
+				if (lowercase_zodis_2[i] == '.') {
+					paskutinio_tasko_indeksas = i;
+				}
+			}
+
+			if (paskutinio_tasko_indeksas == -1) {
+				continue; //jei nera tasko, tai ne URL
+			}
+
+			string pabaiga = "";
+			for (int i = paskutinio_tasko_indeksas + 1; i < lowercase_zodis_2.size(); i++) {
+				if (lowercase_zodis_2[i] == '/') break;
+				pabaiga += lowercase_zodis_2[i];
+			}
+
+			if (pabaiga.empty()) {
+				continue;
+			}
+
+			if (pletiniai.count(pabaiga)) {
+				rasti_url.insert(url_kunas);
+			}
+		}
+	}
+	F2.close();
+
+	//isvedame rastus url'us i faila
+	ofstream U("rez_urls.txt");
+
+	U << "Rasti URL adresai:\n\n";
+
+	for (const auto& url : rasti_url)
+		U << url << endl;
+
+	U.close();
+	cout << "Rezultatai isvesti i failus rez.txt ir rez_urls.txt" << endl;
+	return 0;
+}
+
+
+int utf8_ilgis(const string& zodis) {
+	int sk = 0;
+	for (unsigned char raide : zodis) {
+		//tikrinama, ar ne ASCII simbolis ir tada ar ne antras (continuation) baitas
+		if ((raide & 0xC0) != 0x80) {
+			sk++;
+		}
+	}
+	return sk;
+}
+
+void zodziu_ir_tarpu_spausdinimas(ostream& out, const string& zodis, int sk) {
+	int len = utf8_ilgis(zodis);
+	out << zodis;
+	for (int i = len; i < sk; i++)
+		out << ' ';
 }
